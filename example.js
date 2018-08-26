d4de2406c79f072687ce187874b137895225dece
Anuj Jain <lookforaj@gmail.com>
2018-08-26 19:58:15 +0530

    Added support for promises and callbacks

--- example.js
+++ example.js
@@ -1,2 +1,7 @@
 const indian = require('./index.js');
-indian.convert('౭౩౧౫', 'telugu', 'english');
+indian.convert('౭౩౧౫aaaaa', 'telugu', 'english', function(err, res) {
+    if(err)
+        console.log(err);
+    else
+        console.log(res);
+});
--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 "use strict";
 
 module.exports = {
-    convert: function (number, from, to) {
-        
-        const numberMap = {
-            bengali: "০১২৩৪৫৬৭৮৯",
-            english : "0123456789",
-            gujarati: "૦૧૨૩૪૫૬૭૮૯",
-            hindi : "०१२३४५६७८९",
-            kannada: "೦೧೨೩೪೫೬೭೮೯",
-            konkani: "०१२३४५६७८९",
-            malayalam: "൦൧൨൩൪൫൬൭൮൯",
-            punjabi: "੦੧੨੩੪੫੬੭੮੯",
-            tamil: "௰௧௨௩௪௫௬௭௮௯",
-            telugu: "౦౧౨౩౪౫౬౭౮౯"
+    convert: function (number, from, to, callback) {
+        return new Promise((resolve, reject) => {
+            const numberMap = {
+                bengali: "০১২৩৪৫৬৭৮৯",
+                english: "0123456789",
+                gujarati: "૦૧૨૩૪૫૬૭૮૯",
+                hindi: "०१२३४५६७८९",
+                kannada: "೦೧೨೩೪೫೬೭೮೯",
+                konkani: "०१२३४५६७८९",
+                malayalam: "൦൧൨൩൪൫൬൭൮൯",
+                punjabi: "੦੧੨੩੪੫੬੭੮੯",
+                tamil: "௰௧௨௩௪௫௬௭௮௯",
+                telugu: "౦౧౨౩౪౫౬౭౮౯"
 
-        };
-        var res = "", toLanguageString= numberMap[to];
-        for(var i=0 ; i<number.length ; i++) {
-            var index = numberMap[from].indexOf(number[i]);
-            res = res + toLanguageString[index];
-        }
-        console.log(res);
+            };
+            var res = "", toLanguageString = numberMap[to];
+            if (toLanguageString === undefined) {
+                reject('To language invalid');
+                return callback('To language invalid');
+            }
+
+            for (var i = 0; i < number.length; i++) {
+                var index = numberMap[from].indexOf(number[i]);
+                if (index == -1) {
+                    reject('Invalid input');
+                    return callback('Invalid input');
+                }
+                res = res + toLanguageString[index];
+            }
+            resolve(res);
+            return callback(null, res);
+        }).catch(() => { });
     }
 }
